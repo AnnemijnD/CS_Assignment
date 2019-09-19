989721550b22ff4b691c0ec20b3313a37a7577f2
Annemijn Dijkhuis <annemijndijkhuis@wcw-stud-145-109-32-129.wireless.uva.nl>
2019-09-19 16:32:55 +0200

    Commit

---
+++ plot.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('istherecorrelation.csv', sep=";", decimal=',')
+
+ax = plt.gca()
+
+df.plot(kind='line',x='Year',y='WO [x1000]',ax=ax)
+df.plot(kind='line',x='Year',y='NL Beer consumption [x1000 hectoliter]', color='red', secondary_y=True,ax=ax)
+#
+file = "figure.png"
+plt.savefig(file, dpi=300, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format=None,
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+plt.show()
